956c582d749692c39900546d6d6cf30bb334bd43
Ubuntu <ubuntu@ip-172-31-17-63.us-west-2.compute.internal>
2025-11-09 14:38:42 +0000

    first commit

---
+++ catkinws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/project2/catkinws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/usr/local/cuda-12.2/lib64:'
+export PWD='/home/ubuntu/project2/catkinws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ubuntu/project2/catkinws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ubuntu/project2/catkinws/src:$ROS_PACKAGE_PATH"
---
+++ catkinws/build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/project2/catkinws/src/my_robot
+# Build directory: /home/ubuntu/project2/catkinws/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
